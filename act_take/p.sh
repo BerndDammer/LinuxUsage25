4e5ad4d776d901066987bb6fccbde48347c1365c
BerndDammer <BerndDammer@web.de>
2015-09-10 09:09:23 +0000

    all start changes

--- act_take/p.sh
+++ act_take/p.sh
@@ -1,3 +1,8 @@
 scp put/ping_test.sh       root@10.217.188.200:/lib/systemd
 scp put/promisc.service    root@10.217.188.200:/lib/systemd/system
 scp put/promiscpre.service root@10.217.188.200:/lib/systemd/system
+scp put/lightcontrolsoftware.service root@10.217.188.200:/lib/systemd/system
+
+scp put/imx6/promisc.service   root@10.217.188.100:/lib/systemd/system
+scp put/imx6/httpd.service     root@10.217.188.100:/lib/systemd/system
+scp put/imx6/one               root@10.217.188.100:/home
--- act_take/t.sh
+++ act_take/t.sh
@@ -1,8 +1,10 @@
 rm -r imx5/*
 rm -r imx6/*
+rm -r act/*
 
 mkdir imx5
 mkdir imx6
+mkdir act 
 
 scp -r root@10.217.188.200:/lib/systemd/* imx5
 scp -r root@10.217.188.100:/lib/systemd/* imx6
@@ -17,4 +19,8 @@ scp root@10.217.188.200:/lib/systemd/system/iomaster.service act
 scp root@10.217.188.200:/lib/systemd/system/lightcontrolsoftware.service act
 scp root@10.217.188.200:/lib/systemd/system/lockcontrolsoftware.service act
 scp root@10.217.188.200:/lib/systemd/system/promisc.service act
+
+
+mkdir imx6/home
+scp -r root@10.217.188.100:/home/one imx6/home/
 
