b41b4cf6b2ffff14c3c4984b36803a7d3fe435c1
BerndDammer <BerndDammer@web.de>
2015-09-10 08:39:28 +0000

    add ping test

--- act_take/p.sh
+++ act_take/p.sh
@@ -1,2 +1,3 @@
-scp act/lockcontrolsoftware.service root@10.217.193.200:/lib/systemd/system
-
+scp put/ping_test.sh       root@10.217.188.200:/lib/systemd
+scp put/promisc.service    root@10.217.188.200:/lib/systemd/system
+scp put/promiscpre.service root@10.217.188.200:/lib/systemd/system
